358d1d8c125edf5a20272274b8be9ae67a3cf96b
Danilo Lacerda <danilolacerda@Danilos-MacBook-Pro.local>
2019-07-23 19:43:41 -0300

    termino aula 7

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,9 +1,8 @@
-var pacientes = document.querySelectorAll(".paciente");
+var tabela = document.querySelector("table");
 
-pacientes.forEach(function(paciente) {
-    paciente.addEventListener("dblclick", function() {
-        console.log("Fui clicado com double click");
-        this.remove();
-    });
-
+tabela.addEventListener("dblclick", function(event) {
+    event.target.parentNode.classList.add("fadeOut");
+    setTimeout(function() {
+        event.target.parentNode.remove();
+    }, 500);
 });
